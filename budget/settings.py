9672de275fee0a99217f54774cd55c11b970a1e3
PresterJuan <will.shellenberger@me.com>
2018-05-09 21:49:09 +0000

    Adding humanize so that I can have commas in the templates, also adding the site to settings allowed domains. I still need to play with the javascript for the buttons for each line item.

--- budget/settings.py
+++ budget/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = ')45!0n*+y#y(o4w_q^uqxz0(+zt6@!k%h358#$z$k_2wnmethz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.pleasantonpark.com']
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'budgetApp',
 ]
 
